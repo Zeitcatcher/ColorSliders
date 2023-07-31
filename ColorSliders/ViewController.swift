997734a944ba716ca121be541759d01130678291
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-07-30 22:41:00 -0700

    Everything works correctly. Done

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+        colorSetup()
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    @IBAction func rgbSliderChange(_ sender: UISlider) {
+        colorSetup()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: sender)
+        case greenSlider:
+            greenLabel.text = string(from: sender)
+        default:
+            blueLabel.text = string(from: sender)
+        }
+    }
 }
 
+extension ViewController {
+    private func string(from slider: UISlider) -> String {
+        let textSliderValue = String(format: "%.2f", slider.value)
+        
+        return textSliderValue
+    }
+}
+
+extension ViewController {
+    private func colorSetup() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+}
